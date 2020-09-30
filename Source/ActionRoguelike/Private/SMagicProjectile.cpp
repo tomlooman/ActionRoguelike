61668b8c42a925a73002c5e4f1682436219067a8
Tom Looman <3748238+tomlooman@users.noreply.github.com>
2020-09-30 21:31:18 +0200

    Lecture 8 Results (Example Materials from class, projectile fix)

--- Source/ActionRoguelike/Private/SMagicProjectile.cpp
+++ Source/ActionRoguelike/Private/SMagicProjectile.cpp
@@ -30,7 +30,7 @@ ASMagicProjectile::ASMagicProjectile()
 
 void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (OtherActor)
+	if (OtherActor && OtherActor != GetInstigator())
 	{
 		USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(OtherActor->GetComponentByClass(USAttributeComponent::StaticClass()));
 		if (AttributeComp)
--- Source/ActionRoguelike/Private/STargetDummy.cpp
+++ Source/ActionRoguelike/Private/STargetDummy.cpp
@@ -19,5 +19,8 @@ ASTargetDummy::ASTargetDummy()
 
 void ASTargetDummy::OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewHealth, float Delta)
 {
-
+	if (Delta < 0.0f)
+	{
+		MeshComp->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->TimeSeconds);
+	}
 }
