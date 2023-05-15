e9948285ee5bae1e931431e7994ced0f447a96bc
Tom Looman <3748238+tomlooman@users.noreply.github.com>
2023-05-15 07:25:30 +0200

    Set IncludeOrderVersion to latest (compile performance)
    Explicitly setting IncludeOrderVersion to latest in build target, improving compile times. (~15-20% on notebook)

--- Source/ActionRoguelike.Target.cs
+++ Source/ActionRoguelike.Target.cs
@@ -9,6 +9,7 @@ public class ActionRoguelikeTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "ActionRoguelike" } );
 	}
--- Source/ActionRoguelikeEditor.Target.cs
+++ Source/ActionRoguelikeEditor.Target.cs
@@ -9,6 +9,7 @@ public class ActionRoguelikeEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "ActionRoguelike" } );
 	}
