42cc469f089abdb1f7e28f87fe9cdb98c5124bb9
Tom Looman <3748238+tomlooman@users.noreply.github.com>
2025-08-08 17:18:16 +0200

    Added interaction component find best interactable

--- Source/ActionRoguelike/Player/RogueInteractionComponent.cpp
+++ Source/ActionRoguelike/Player/RogueInteractionComponent.cpp
@@ -2,6 +2,7 @@
 
 
 #include "RogueInteractionComponent.h"
+#include "Engine/OverlapResult.h"
 
 
 URogueInteractionComponent::URogueInteractionComponent()
@@ -17,7 +18,44 @@ void URogueInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickT
 	APlayerController* PC = CastChecked<APlayerController>(GetOwner());
 
 	FVector Center = PC->GetPawn()->GetActorLocation();
-	
-	DrawDebugBox(GetWorld(), Center, FVector(20.0f), FColor::Red);
+
+	ECollisionChannel CollisionChannel = ECC_Visibility;
+
+	FCollisionShape Shape;
+	Shape.SetSphere(InteractionRadius);
+
+	TArray<FOverlapResult> Overlaps;
+	GetWorld()->OverlapMultiByChannel(Overlaps, Center, FQuat::Identity, CollisionChannel, Shape);
+
+	DrawDebugSphere(GetWorld(), Center, InteractionRadius, 32, FColor::White);
+
+	AActor* BestActor = nullptr;
+	float HighestDotResult = -1.0;
+
+	for (FOverlapResult& Overlap : Overlaps)
+	{
+		FVector OverlapLocation = Overlap.GetActor()->GetActorLocation();
+		
+		DrawDebugBox(GetWorld(), OverlapLocation, FVector(50.0f), FColor::Red);
+
+		FVector OverlapDirection = (OverlapLocation - Center).GetSafeNormal();
+
+		float DotResult = FVector::DotProduct(OverlapDirection, PC->GetControlRotation().Vector());
+
+		FString DebugString = FString::Printf(TEXT("Dot: %f"), DotResult);
+
+		DrawDebugString(GetWorld(), OverlapLocation, DebugString, nullptr, FColor::White, 0.0f, true);
+
+		if (DotResult > HighestDotResult)
+		{
+			BestActor = Overlap.GetActor();
+			HighestDotResult = DotResult;
+		}
+	}
+
+	if (BestActor)
+	{
+		DrawDebugBox(GetWorld(), BestActor->GetActorLocation(), FVector(60.0f), FColor::Green);
+	}
 }
 
--- Source/ActionRoguelike/Player/RogueInteractionComponent.h
+++ Source/ActionRoguelike/Player/RogueInteractionComponent.h
@@ -12,6 +12,11 @@ class ACTIONROGUELIKE_API URogueInteractionComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category="Interaction")
+	float InteractionRadius = 800;
+
 public:
 
 	URogueInteractionComponent();
