063ae0dd11e0ac227833dac4a1592996f684b39d
tomlooman <3748238+tomlooman@users.noreply.github.com>
2026-03-06 14:11:51 +0100

    Expanded on the target dummy
    target dummy can be used again for impacts and added the hitflashes and hit reactions

--- Source/ActionRoguelike/World/RogueTargetDummy.cpp
+++ Source/ActionRoguelike/World/RogueTargetDummy.cpp
@@ -6,14 +6,20 @@
 #include "SharedGameplayTags.h"
 #include "ActionSystem/RogueActionComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Core/RogueMonsterData.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(RogueTargetDummy)
 
 
 ARogueTargetDummy::ARogueTargetDummy()
 {
+	DefaultSceneRootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	RootComponent = DefaultSceneRootComp;
+	
 	SkelMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComp"));
-	RootComponent = SkelMeshComp;
+	SkelMeshComp->SetRelativeLocation(FVector(0,0,-90.f));
+	SkelMeshComp->SetRelativeRotation(FRotator(0, -90, 0));
+	SkelMeshComp->SetupAttachment(DefaultSceneRootComp);
 
 	ActionComp = CreateDefaultSubobject<URogueActionComponent>(TEXT("ActionComp"));
 	ActionComp->SetDefaultAttributeSet(URogueHealthAttributeSet::StaticClass());
@@ -27,11 +33,32 @@ void ARogueTargetDummy::PostInitializeComponents()
 	ActionComp->GetAttributeListenerDelegate(SharedGameplayTags::Attribute_Health).AddUObject(this, &ThisClass::OnHealthChanged);
 }
 
+bool ARogueTargetDummy::AddImpulseAtLocationCustom(FVector Impulse, FVector Location, FName BoneName)
+{
+	FHitReactConfig& Config = ReferenceMonsterData->HitReactions;
+	// Play hit Anim
+	SkelMeshComp->GetAnimInstance()->PlaySlotAnimationAsDynamicMontage(
+		Config.GetAnimFromAngle(this, Impulse.GetSafeNormal()),
+		Config.SlotName,
+		Config.BlendInTime,
+		Config.BlendOutTime);
+
+	return true;	
+}
+
 
 void ARogueTargetDummy::OnHealthChanged(float NewValue, const FAttributeModification& AttributeMod)
 {
-	if (AttributeMod.Magnitude < 0.0f)
+	// Read by the Overlay Material to flash
+	SkelMeshComp->SetCustomPrimitiveDataFloat(HitFlash_CustomPrimitiveIndex, GetWorld()->TimeSeconds);
+
+	// We can skip rendering this at a distance
+	/*SkelMeshComp->SetOverlayMaterialMaxDrawDistance(CachedOverlayMaxDistance);
+
+	// After 1.0seconds we should be finished with the hitflash (re-use the handle to reset timer if we get hit again)
+	GetWorldTimerManager().SetTimer(OverlayTimerHandle, [this]()
 	{
-		SkelMeshComp->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->TimeSeconds);
-	}
+		// Cheap trick to skip rendering this all the time unless we are actively hit flashing
+		SkelMeshComp->SetOverlayMaterialMaxDrawDistance(1);
+	}, 1.0f, false);*/
 }
--- Source/ActionRoguelike/World/RogueTargetDummy.h
+++ Source/ActionRoguelike/World/RogueTargetDummy.h
@@ -4,15 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "ActionSystem/RogueActionSystemInterface.h"
+#include "Core/RogueGameplayInterface.h"
 #include "GameFramework/Actor.h"
 #include "RogueTargetDummy.generated.h"
 
+class URogueMonsterData;
 struct FAttributeModification;
 class URogueActionComponent;
 
 
 UCLASS()
-class ACTIONROGUELIKE_API ARogueTargetDummy : public APawn, public IRogueActionSystemInterface
+class ACTIONROGUELIKE_API ARogueTargetDummy : public APawn, public IRogueActionSystemInterface, public IRogueGameplayInterface
 {
 	GENERATED_BODY()
 
@@ -24,6 +26,17 @@ protected:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USkeletalMeshComponent> SkelMeshComp;
 
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USceneComponent> DefaultSceneRootComp;
+
+	/* Index must match the CustomPrimitiveData index used in the Overlay material */
+	UPROPERTY(VisibleAnywhere, Category=TargetDummy)
+	int32 HitFlash_CustomPrimitiveIndex = 0;
+
+	/* Re-use some monster data such as hit reactions */
+	UPROPERTY(EditAnywhere, Category=TargetDummy)
+	TObjectPtr<URogueMonsterData> ReferenceMonsterData;
+	
 	void OnHealthChanged(float NewValue,  const FAttributeModification& AttributeMod);
 	
 public:	
@@ -31,10 +44,11 @@ public:
 	ARogueTargetDummy();
 
 	virtual void PostInitializeComponents() override;
+	
+	virtual bool AddImpulseAtLocationCustom(FVector Impulse, FVector Location, FName BoneName = NAME_None) override;
 
 	virtual URogueActionComponent* GetActionComponent() const override
 	{
 		return ActionComp;
 	}
-
 };
