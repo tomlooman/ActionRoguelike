8ecc99247b548b225c65624d11fcc01835a80bbe
Tom Looman <3748238+tomlooman@users.noreply.github.com>
2026-05-14 12:49:49 +0200

    Added stun from heavy damage debuff to minion

---
+++ Source/ActionRoguelike/ActionSystem/RogueActionEffect_StunFromDamage.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueActionEffect_StunFromDamage.h"
+
+#include "ActionRoguelike.h"
+#include "RogueActionComponent.h"
+#include "SharedGameplayTags.h"
+#include "Logging/StructuredLog.h"
+
+
+
+void URogueActionEffect_StunFromDamage::StartAction_Implementation(AActor* Instigator)
+{
+	Super::StartAction_Implementation(Instigator);
+	
+	GetOwningComponent()->GetAttributeListenerDelegate(SharedGameplayTags::Attribute_Health).AddUObject(this, 
+		&ThisClass::OnHealthChanged);
+}
+
+
+void URogueActionEffect_StunFromDamage::OnHealthChanged(float NewValue, const FAttributeModification& AttriMod)
+{
+	if (AttriMod.Magnitude >= 0.0f)
+	{
+		// ignore healing
+		return;
+	}
+	
+	const float TimeNow = GetWorld()->TimeSeconds;
+	// Did less time pass than the threshold?
+	const float DeltaTime = TimeNow - LastDamageTime;
+	
+	if (DeltaTime < TimeDeltaThreshold)
+	{
+		SummedRecentDamage += FMath::Abs(AttriMod.Magnitude);
+	}
+	else
+	{
+		SummedRecentDamage = FMath::Abs(AttriMod.Magnitude);
+	}
+	
+	LastDamageTime = TimeNow;
+	
+	// Trigger stun?
+	if (SummedRecentDamage >= DamageThreshold)
+	{
+		GetOwningComponent()->AddAction(AttriMod.Instigator.Get(), EffectOnThreshold);
+		
+		UE_LOG(LogGame, Log, TEXT("Stunned applied to %s"), *GetNameSafe(GetOwningComponent()->GetOwner()));
+		
+		//UE_LOGFMT(LogGame, Log, "Stunned Debuff applied to {owningActor}", GetNameSafe(GetOwningComponent()->GetOwner());
+		
+		// Reset
+		SummedRecentDamage = 0.0f;
+		LastDamageTime = 0.0f;
+	}
+}
---
+++ Source/ActionRoguelike/ActionSystem/RogueActionEffect_StunFromDamage.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RogueActionEffect.h"
+#include "RogueActionEffect_StunFromDamage.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class ACTIONROGUELIKE_API URogueActionEffect_StunFromDamage : public URogueActionEffect
+{
+	GENERATED_BODY()
+	
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category=Effect)
+	float DamageThreshold = 20.0f;
+	
+	UPROPERTY(EditDefaultsOnly, Category=Effect)
+	TSubclassOf<URogueActionEffect> EffectOnThreshold;
+	
+	UPROPERTY(EditDefaultsOnly, Category=Effect)
+	float TimeDeltaThreshold = 1.0f;
+	
+	float LastDamageTime = 0.0f;
+	
+	float SummedRecentDamage = 0.0f;
+	
+	UFUNCTION()
+	void OnHealthChanged(float NewValue, const FAttributeModification& AttriMod);
+	
+public:
+	
+	virtual void StartAction_Implementation(AActor* Instigator) override;
+};
